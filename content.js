917591b34b00fc719d3d48ef3888b215e9c2657e
Kerri Miller <kerrizor@github.com>
2017-01-02 05:49:50 -0800

    Add case matching helper and bump version

--- content.js
+++ content.js
@@ -1,3 +1,20 @@
+function matchCase(text, pattern) {
+    var result = '';
+
+    for(var i = 0; i < text.length; i++) {
+        var c = text.charAt(i);
+        var p = pattern.charCodeAt(i);
+
+        if(p >= 65 && p < 65 + 26) {
+            result += c.toUpperCase();
+        } else {
+            result += c.toLowerCase();
+        }
+    }
+
+    return result;
+}
+
 var elements = document.getElementsByTagName('*');
 
 for (var i = 0; i < elements.length; i++) {
@@ -8,7 +25,9 @@ for (var i = 0; i < elements.length; i++) {
 
         if (node.nodeType === 3) {
             var text = node.nodeValue;
-            var replacedText = text.replace(/business/gi, 'burrito');
+            var replacedText = text.replace(/business/gi, function(match) {
+              return matchCase("burrito", match);
+            });
 
             if (replacedText !== text) {
                 element.replaceChild(document.createTextNode(replacedText), node);
